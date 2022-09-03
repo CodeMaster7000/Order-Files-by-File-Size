0634f92c3b8985da43172860cb10f9ade5d6cda3
CodeMaster7000 <95772109+CodeMaster7000@users.noreply.github.com>
2022-09-03 16:30:40 +0100

    Update glob() method.py

--- glob() method.py
+++ glob() method.py
@@ -7,3 +7,7 @@ list_of_files = filter( os.path.isfile,
 
 list_of_files = sorted( list_of_files,
                         key =  lambda x: os.stat(x).st_size)
+
+for path_of_file in list_of_files:
+    size_of_file  = os.stat(path_of_file).st_size 
+    print(size_of_file, ' -->', path_of_file) 
